56bd10c8d7a883636e2c1269c9409ab07c824d23
Christian Broberg <cb@cb-pro.local>
2018-11-12 22:36:50 +0100

    Template update with authentication

--- api/template.js
+++ api/template.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const verifyAPIVersion = require('senti-apicore').verifyapiversion
+const { authenticate } = require('senti-apicore')
 
 /* get template */
 router.get('/:version/', async (req, res, next) => {
-	if (verifyAPIVersion(req.params.version)) {
-		res.json('SentiAPI template')
+	let apiVersion = req.params.version
+	let authToken = req.headers.auth
+	if (verifyAPIVersion(apiVersion)) {
+		if (authenticate(authToken)) {
+			res.json('SentiAPI template')
+			console.log('API Access Authenticated!')
+		} else {
+			res.json('Unauthorized Access!')
+			console.log('Unauthorized Access!')
+		}
 	} else {
 		res.send(`API/template version: ${req.params.version} not supported`)
 	}
