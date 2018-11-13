b92490d323e27142c591e45f2ef81f4cf4824765
Christian Broberg <cb@cb-pro.local>
2018-11-13 21:55:08 +0100

    Travis deployment with split deploy script ... you better work now!

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-echo
-echo Deploying Senti API $1 ... 
-rsync -r --quiet $2/ deploy@organa.webhouse.net:/srv/nodejs/senti/senti-api-dev
-echo
-echo Restarting Senti API $1 service ... 
-ssh deploy@organa.webhouse.net 'sudo /srv/nodejs/api-restart.sh dev'
-echo
-echo Deployment done!
+if [[ "$1" == "master" ]]; then 
+	echo
+	echo Deploying Senti API $1 ... 
+	rsync -r --quiet $2/ deploy@organa.webhouse.net:/srv/nodejs/senti/senti-api
+	echo
+	echo Restarting Senti API production service ... 
+	ssh deploy@organa.webhouse.net 'sudo /srv/nodejs/api-restart.sh master'
+	echo
+	echo Deployment to production & restart done!
+fi 
+
+if [[ "$1" == "dev" ]]; then 
+	echo
+	echo Deploying Senti API $1 ... 
+	rsync -r --quiet $2/ deploy@organa.webhouse.net:/srv/nodejs/senti/senti-api-dev
+	echo
+	echo Restarting Senti API $1 service ... 
+	ssh deploy@organa.webhouse.net 'sudo /srv/nodejs/api-restart.sh dev'
+	echo
+	echo Deployment to dev & restart done!
+fi
