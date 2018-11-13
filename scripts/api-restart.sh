48a20801c1491cb103206260995cd11d9a909322
Christian Broberg <cb@cb-pro.local>
2018-11-14 00:27:05 +0100

    Script cleanup

--- scripts/api-restart.sh
+++ scripts/api-restart.sh
@@ -4,7 +4,7 @@
 if [[ "$1" == "master" ]]; then 
 	npm install --prefix /srv/nodejs/senti/senti-api
 	systemctl restart senti-api.service
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti API on PRODUCTION updated and restarted!"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti API MASTER updated and restarted!"}' https://hooks.slack.com/services/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
 	echo
 	exit 0
 fi
@@ -12,7 +12,7 @@ fi
 if [[ "$1" == "dev" ]]; then 
 	npm install --prefix /srv/nodejs/senti/senti-api-dev
 	systemctl restart senti-api-dev.service
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti API on DEV updated and restarted!"}' https://hooks.slack.com/services/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
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti API DEV updated and restarted!"}' https://hooks.slack.com/services/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
 	echo
 	exit 0
 fi
