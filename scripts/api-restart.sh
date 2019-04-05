afb5eea79d1c14ab2d5ec7e4b62599e476d12a3d
Christian Broberg <cb@cb-pro.local>
2019-04-05 20:57:52 +0200

    Added new Slack integration and workspace

--- scripts/api-restart.sh
+++ scripts/api-restart.sh
@@ -4,7 +4,10 @@
 if [[ "$1" == "master" ]]; then 
 	npm install --prefix /srv/nodejs/senti/senti-api
 	systemctl restart senti-api.service
+	# WebHouse Workspace
 	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti API MASTER updated and restarted!"}' https://hooks.slack.com/services/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
+	# Senti Workspace
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti API MASTER updated and restarted!"}' https://hooks.slack.com/services/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
 	echo
 	exit 0
 fi
@@ -12,9 +15,14 @@ fi
 if [[ "$1" == "dev" ]]; then 
 	npm install --prefix /srv/nodejs/senti/senti-api-dev
 	systemctl restart senti-api-dev.service
+	# WebHouse Workspace
 	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti API DEV updated and restarted!"}' https://hooks.slack.com/services/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
+	# Senti Workspace
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti API DEV updated and restarted!"}' https://hooks.slack.com/services/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
 	echo
 	exit 0
 fi
 
 exit 0
+
+
