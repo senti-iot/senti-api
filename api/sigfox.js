808235ffa4b6ad20389b6802e2bea41450274ce1
Andrei Tudor <andrei.tudor27@gmail.com>
2019-04-08 16:31:07 +0200

    Sigfox

--- api/sigfox.js
+++ api/sigfox.js
@@ -11,19 +11,40 @@ router.get('/:version/', async (req, res, next) => {
 	// res.send('API Test Works')
 	if (verifyAPIVersion(apiVersion)) {
 		if (authenticate(authToken)) {
-			res.json('API/template Access Authenticated!')
+			res.json('API/sigfox GET Access Authenticated!')
 			// res.json({res, req})
 			console.clear()
-			console.log('API/template Access Authenticated!')
+			console.log('API/sigfox GET Access Authenticated!')
 		} else {
 			res.status(403).json('Unauthorized Access! 403')
 			// res.json('Unauthorized Access!')
 			console.log('Unauthorized Access!')
 		}
 	} else {
-		console.log(`API/template version: ${apiVersion} not supported`)
-		res.send(`API/template version: ${apiVersion} not supported`)
+		console.log(`API/sigfox version: ${apiVersion} not supported`)
+		res.send(`API/sigfox version: ${apiVersion} not supported`)
 	}
 })
 
+router.post('/:version', async (req,res, next)=> {
+	let apiVersion = req.params.version
+	let authToken = req.headers.auth
+	console.log(req.body)
+	// console.log(req, res)
+	// res.send('API Test Works')
+	if (verifyAPIVersion(apiVersion)) {
+		if (authenticate(authToken)) {
+			res.json('API/sigfox POST Access Authenticated!')
+			// res.json({res, req})
+			console.log('API/sigfox POST Access Authenticated!')
+		} else {
+			res.status(403).json('Unauthorized Access! 403')
+			// res.json('Unauthorized Access!')
+			console.log('Unauthorized Access!')
+		}
+	} else {
+		console.log(`API/sigfox version: ${apiVersion} not supported`)
+		res.send(`API/sigfox version: ${apiVersion} not supported`)
+	}
+})
 module.exports = router
