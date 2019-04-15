c8c983624f45fab096f5c1387f5ec738b83cc72b
Christian Broberg <cb@cb-pro.local>
2019-04-15 21:32:37 +0200

    Cleaning

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,9 @@
 if [[ "$1" == "master" ]]; then 
 	echo
 	echo Deploying Senti API $1 ... 
-	# rsync -r --quiet $2/ deploy@organa.webhouse.net:/srv/nodejs/senti/senti-api
 	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/senti-api
 	echo
 	echo Restarting Senti API production service ... 
-	# ssh deploy@organa.webhouse.net 'sudo /srv/nodejs/api-restart.sh master'
-	# ssh deploy@organa.webhouse.net 'sudo /srv/nodejs/senti/senti-api/scripts/api-restart.sh master'
 	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/senti-api/scripts/api-restart.sh master'
 	echo
 	echo Deployment to production and restart done!
@@ -18,12 +15,9 @@ fi
 if [[ "$1" == "dev" ]]; then 
 	echo
 	echo Deploying Senti API $1 ... 
-	# rsync -r --quiet $2/ deploy@organa.webhouse.net:/srv/nodejs/senti/senti-api-dev
 	rsync -r --quiet $2/ deploy@rey.webhouse.net:/srv/nodejs/senti/senti-api-dev
 	echo
 	echo Restarting Senti API $1 service ... 
-	# ssh deploy@organa.webhouse.net 'sudo /srv/nodejs/api-restart.sh dev'
-	# ssh deploy@organa.webhouse.net 'sudo /srv/nodejs/senti/senti-api-dev/scripts/api-restart.sh dev'
 	ssh deploy@rey.webhouse.net 'sudo /srv/nodejs/senti/senti-api-dev/scripts/api-restart.sh dev'
 	echo
 	echo Deployment to dev and restart done!
